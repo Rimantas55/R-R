cd35812bd519432011bbf9240ba01b0a4d18e7b9
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-01-09 18:58:29 +0200

    Done History
    Left to do R&R

--- history.php
+++ history.php
@@ -6,15 +6,110 @@
 
 	<title>Points History</title>
 </head>
-<body>
+<body id="history">
 
+	<a href="index.php">Back to main</a>
+	<p>new activities on top</p>	
+	<div class="container">
+		<table>
+			<thead>
+				<tr>
+					<th>
+						Nr./ ID
+					</th>
+					<th>
+						Date
+					</th>
+					<th>
+						Received || Spent points
+					</th>
+					<th>
+						Total points
+					</th>
+					<th>
+						Points received / Spend FOR
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>
+						1.
+					</td>
+					<td>
+						2018-12-26 12:18:17
+					</td>
+					<td>
+						<p class="received_points">+100</p>
+					</td>
+					<td>
+						<p class="total_points">1000</p>
+					</td>
+					<td>
+						<div class="history_checks">
+							<div class="exampleCheck1">
+								<input type="checkbox" class="form-check-input" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+							</div>
+							<div class="exampleCheck1">
+								<input type="checkbox" class="form-check-input" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+							</div>
+							<div class="exampleCheck1">
+								<input type="checkbox" class="form-check-input" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+							</div>
+							<div class="exampleCheck1">
+								<input type="checkbox" class="form-check-input" id="exampleCheck1">
+								<label class="form-check-label" for="exampleCheck1">Check me out</label>
+							</div>
+						</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						2.
+					</td>
+					<td>
+						2019-01-01 08:47:01
+					</td>
+					<td>
+						<p class="spent_points">-100</p>
+					</td>
+					<td>
+						<p class="total_points">900</p>
+					</td>
+					<td>
+						<img src="images/scooter.jpg">
+						<p>Cashed out el-Scooter</p>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						3.
+					</td>
+					<td>
+						2019-01-01 08:47:01
+					</td>
+					<td>
+						<p class="spent_points">-100</p>
+					</td>
+					<td>
+						<p class="total_points">800</p>
+					</td>
+					<td>
+						<img src="images/chocolate.jpg">
+						<p>Cashed out chocolate</p>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+		<a href="#">Conatct R&R admin</a> <br>
+		<a href="#">Conatct your manager</a>
+
+	</div>
 	
 
-	<p>new activities on top</p>
-	<p>Date</p>	
-	<p>Points received/spent</p>
-	<p>Total points (That day after aproving or spending)</p>
-	<p>Points received spend for cia bus foto arba veiklos surasytos</p>
 
 
 </body>
